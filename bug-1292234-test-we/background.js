77372655218b45ba711415f502cc9e136ae45a77
Bianca Danforth <biancadanforth@gmail.com>
2019-03-06 14:25:54 -0800

    Put something in window.indexedDB storage for testing
    In ./devtools/shared/specs/storage.js, the `storeObjectType` provides a schema of sorts our [WIP patch](https://gist.github.com/biancadanforth/fb6aaae07084512a594a8098c971807e), which currently uses an existing `storeObjectType` of `"storagestoreobject"`. This is the `storeObjectType` used by the window.localStorage and window.sessionStorage actors.
    
    However, this `"storeagestoreobject"` type [limits the data types](https://searchfox.org/mozilla-central/source/devtools/shared/specs/storage.js#119-122) of values stored to strings. Extension storage like `browser.storage.local` have fewer restrictions on the types of values that can be stored compared to window.localStorage and window.sessionStorage.
    
    Extension storage key-value type restrictions are most similar to (but more restrictive than[1]) `window.indexedDB`, so this patch puts some items in IndexedDB to compare how the IndexedDB storage actor handles and processes the data as a basis to create a new `storeObjectType` for extension storage.
    
    [1]:
    Compared to [`window.localStorage` and `window.sessionStorage`](https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/API/storage):
    > The values stored can be any JSON-ifiable value, not just String.
    Compared to [`window.indexedDB`](https://developer.mozilla.org/en-US/docs/Web/API/IndexedDB_API#Key_concepts_and_usage):
    While extension storage can store any JSON-ifiable value, IndexedDB can store:
    > any objects supported by the [structured clone algorithm](https://developer.mozilla.org/en-US/docs/Web/Guide/API/DOM/The_structured_clone_algorithm)
    This includes Maps, for example and other complex objects.

--- bug-1292234-test-we/background.js
+++ bug-1292234-test-we/background.js
@@ -1,9 +1,72 @@
 (async function main() {
 
-  browser.storage.local.set({
-    rainbows: "unicorns",
-    catgifs: true,
-  });
+  // This is what our customer data looks like.
+  const customerData = [
+    { ssn: "444-44-4444", name: "Bill", age: 35, email: "bill@company.com" },
+    { ssn: "555-55-5555", name: "Donna", age: 32, email: "donna@home.org" }
+  ];
+  
+  // Populate extension local storage as similarly as possible to how the
+  // same data populates IndexedDB storage
+  for (const entry of customerData) {
+    browser.storage.local.set({[entry.ssn]: entry});
+  }
+
+  // Populate window.indexedDB storage
+  // (Put something (e.g. object literal) in window.indexedDB to compare
+  // how IndexedDB storage actors access/handle the information, since
+  // extension storage is similar (but more restrictive) in terms of
+  // acceptable store items.)
+  function storeObjectLiteralInIndexedDB() {
+
+    if (!window.indexedDB) {
+      window.alert("Your browser doesn't support a stable version of IndexedDB.");
+    }
+
+    // Open a database
+    const dbName = "Bug1292234IndexedDB";
+
+    var db;
+    var request = indexedDB.open(dbName, 4);
+    request.onerror = function(event) {
+      throw new Error(`${request.errorCode}`);
+    };
+    request.onsuccess = function(event) {
+      db = event.target.result;
+    };
+    // Also fires when a new db is being created.
+    request.onupgradeneeded = function(event) {
+      var db = event.target.result;
+
+      // 2. Create an object store in the database
+      // Create an objectStore to hold information about our customers. We're
+      // going to use "ssn" as our key path because it's guaranteed to be
+      // unique - or at least that's what I was told during the kickoff meeting.
+      var objectStore = db.createObjectStore("customers", { keyPath: "ssn" });
+
+      // Create an index to search customers by name. We may have duplicates
+      // so we can't use a unique index.
+      objectStore.createIndex("name", "name", { unique: false });
+
+      // Create an index to search customers by email. We want to ensure that
+      // no two customers have the same email, so use a unique index.
+      objectStore.createIndex("email", "email", { unique: true });
+
+      // Wait for the operation to complete by listening to the right kind of
+      // DOM event.
+      // Use transaction oncomplete to make sure the objectStore creation is 
+      // finished before adding data into it.
+      objectStore.transaction.oncomplete = function(event) {
+        // Store values in the newly created objectStore.
+        var customerObjectStore = db.transaction("customers", "readwrite").objectStore("customers");
+        customerData.forEach((customer) => {
+          customerObjectStore.add(customer);
+        });
+      };
+    };
+  }
+
+  storeObjectLiteralInIndexedDB();
 
   function handleChange(changes, areaName) {
     if (areaName !== 'local') return;
