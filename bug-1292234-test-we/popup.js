f115587bde0005ea41a2d7b7fd9fad34c43fc6e6
Bianca Danforth <biancadanforth@gmail.com>
2019-03-22 11:05:08 -0700

    Open an extension page in popup
    This helps debug the situation where an extension does not have a background script (and may therefore not have a window object available to our storage actor.

--- bug-1292234-test-we/popup.js
+++ bug-1292234-test-we/popup.js
@@ -1,3 +1,10 @@
+// Open an extension page in a new tab to make use of the Browser Content Toolbox
+// to be able to debug the storage actor script:
+// (mozilla-central/devtools/server/actors/storage.js)
+// Useful for debugging storage actor when we have no background page (or remove
+// it temporarily)
+browser.tabs.create({url: browser.extension.getURL('extension-page.html')});
+
 const customerData = [
   { ssn: "666-66-6666", name: "Linda", age: 39, email: "linda@company.com" },
   { ssn: "777-77-7777", name: "Greg", age: 31, email: "greg@home.org" }
